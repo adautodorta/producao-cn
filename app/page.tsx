ca66be181224c5e022bb42660c184048e6b8a7a8
adautodf <adautodortafilho@gmail.com>
2025-04-25 15:04:39 -0300

    feature: melhorando botão de limpar checklist

--- app/page.tsx
+++ app/page.tsx
@@ -83,27 +83,6 @@ export default function Home() {
     localStorage.setItem('checkedTasks', JSON.stringify(dataToSave));
   };
 
-  // const handleClearChecklist = (prefix: 'p1-' | 'stage-') => {
-  //   if (window.confirm('Tem certeza que deseja limpar todas as tarefas desta seção?')) {
-  //     const newCheckedTasks = Object.entries(checkedTasks).reduce((acc, [key, value]) => {
-  //       if (!key.startsWith(prefix)) {
-  //         acc[key] = value;
-  //       }
-  //       return acc;
-  //     }, {} as { [key: string]: boolean });
-  
-  //     setCheckedTasks(newCheckedTasks);
-      
-  //     const dataToSave = {
-  //       tasks: newCheckedTasks,
-  //       date: new Date().toISOString()
-  //     };
-      
-  //     localStorage.setItem('checkedTasks', JSON.stringify(dataToSave));
-  //     setResetKey(prev => prev + 1);
-  //   }
-  // };
-  
   return (
     <div className="overflow-hidden grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center">
@@ -637,16 +616,6 @@ export default function Home() {
             </DialogFooter>
           </DialogContent>
         </Dialog>
-        {/* <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol> */}
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <Button className="w-[38vw] max-w-[192px] sm:w-[192px]">
@@ -663,53 +632,6 @@ export default function Home() {
           </Button>
         </div>
       </main>
-      {/* <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer> */}
     </div>
   );
 }
